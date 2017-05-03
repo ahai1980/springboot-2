3bd59ac296c6e3c7ba3beea3ddb7fc6e26527fd3
jetora <478370215@qq.com>
2017-05-04 00:22:58 +0800

    update swagerconfig

--- src/main/java/com/ora/SwaggerConfig.java
+++ src/main/java/com/ora/SwaggerConfig.java
@@ -23,31 +23,8 @@ public class SwaggerConfig {
     /**
      * 可以定义多个组，比如本类中定义把test和demo区分开了
      * （访问页面就可以看到效果了）
-     *
-     * @Bean public Docket testApi() {
-     * return new Docket(DocumentationType.SWAGGER_2)
-     * .apiInfo(apiInfo()).ignoredParameterTypes(User.class)
-     * .select()
-     * .apis(RequestHandlerSelectors.basePackage("com.ora"))
-     * .paths(PathSelectors.any())
-     * .build();
-     * }
-     * <p>
-     * <p>
-     * <p>
-     * private ApiInfo apiInfo() {
-     * ApiInfo apiInfo = new ApiInfo("SpringBoot学习demo",//大标题
-     * "Spring boot + swagger + mybatis + druid",//小标题
-     * "1.0",//版本
-     * "NO terms of service",
-     * new Contact("jetora","https://github.com/jetora","jetoralook@outlook.com"),//作者
-     * "RayeBlog",//链接显示文字
-     * "http://www.raye.wang/"//网站链接
-     * );
-     * <p>
-     * return apiInfo;
-     * }
      */
+
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
