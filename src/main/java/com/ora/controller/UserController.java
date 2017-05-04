f591b6c35365224c3a33e573c696071fed18e378
jetora <478370215@qq.com>
2017-05-04 19:11:34 +0800

    update swagerconfig

--- src/main/java/com/ora/controller/UserController.java
+++ src/main/java/com/ora/controller/UserController.java
@@ -1,5 +1,10 @@
 package com.ora.controller;
 
+import com.ora.constant.UserErrorInfoEnum;
+import com.ora.result.GlobalErrorInfoEnum;
+import com.ora.result.GlobalErrorInfoException;
+import com.ora.result.ResultBody;
+import org.springframework.util.StringUtils;
 import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -37,8 +42,11 @@ public class UserController {
 		@ApiImplicitParam(name="password",paramType="query",dataType="string")
 	})*/
     @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
-    public String add(@RequestBody User user) {
+    /*public String add(@RequestBody User user)
         return "hello " + userService.add(user);
+    }*/
+    public ResultBody add(@RequestBody User user) {
+        return new ResultBody(userService.add(user));
     }
 
     /**
@@ -49,9 +57,17 @@ public class UserController {
      */
     @RequestMapping("/view/{id}")
     @ApiOperation(notes = "查询用户", value = "查询单个用户", httpMethod = "GET")
+    /*
     public User getCountryById(@PathVariable Integer id) {
         return userService.findByUserId(id);
+    }*/
+    public ResultBody getCountryById(@PathVariable Integer id) throws GlobalErrorInfoException {
+        /*if (id.equals(null)) {
+            throw new GlobalErrorInfoException(UserErrorInfoEnum.PARAMS_NO_COMPLETE);
+        }*/
+        return new ResultBody(userService.findByUserId(id));
     }
+
 
     /**
      * 查询所有用户
@@ -67,7 +83,7 @@ public class UserController {
 
     @ApiOperation(value = "删除用户", notes = "删除用户", httpMethod = "POST")
     @RequestMapping(value = "/delete/{id}")
-    public Boolean deleteUser(@PathVariable Integer id) {
+    /*public Boolean deleteUser(@PathVariable Integer id) {
         boolean result;
         if (userService.findByUserId(id) != null) {
             result = userService.deleteUserByUserId(id) > 0;
@@ -75,5 +91,12 @@ public class UserController {
             result = false;
         }
         return result;
+    }*/
+    public ResultBody deleteUser(@PathVariable Integer id) {
+        if (userService.findByUserId(id) != null) {
+            return new ResultBody(userService.deleteUserByUserId(id));
+        }else{
+            return new ResultBody(GlobalErrorInfoEnum.NOT_FOUND);
+        }
     }
 }
